e8f88e6d5a822d408ccd207b9cc869337d677a61
Tristen Everett <spock214@gmail.com>
2020-07-15 10:22:56 -0700

    create constructor for Bear object

---
+++ src/bear.js
@@ -0,0 +1,10 @@
+export class Bear {
+  constructor(name, level){
+    this.name = name;
+    this.difficulty = level;
+    this.interval = 0;
+    this.food = 10;
+    this.sleep = 10;
+    this.mood = 100;
+  }
+}
