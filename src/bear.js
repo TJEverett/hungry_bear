b8d661ab617ad22fa30cafba509e7741e2a9966a
Tristen Everett <spock214@gmail.com>
2020-07-15 10:58:33 -0700

    create setInterval prototype that will set this.interval based on difficulty level and gets called in the constructor

--- src/bear.js
+++ src/bear.js
@@ -1,10 +1,22 @@
 export class Bear {
   constructor(name, level){
+    // level is 1 (easy), 2 (normal), or 3 (hard) exceptions are built as level 2.
     this.name = name;
     this.difficulty = level;
     this.interval = 0;
     this.food = 10;
     this.sleep = 10;
     this.mood = 100;
+    this.setInterval();
+  }
+
+  setInterval() {
+    if(this.difficulty === 1) {
+      this.interval = 3000;
+    }else if (this.difficulty === 3) {
+      this.interval = 1000;
+    }else {
+      this.interval = 2000;
+    }
   }
 }
